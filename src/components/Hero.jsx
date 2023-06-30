183b4529198833a4775216c669643c8bc6c0c14a
iammirzet <82601696+iammirzet@users.noreply.github.com>
2023-06-30 09:32:02 +0200

    sm

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,12 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className={`flex md:flex-row flex-col ${styles.paddingY} sm:mt-33 lg:mt-8 xl:mt-10 `}
+      className={`flex md:flex-row flex-col ${styles.paddingY} sm:mt-12 `}
     >
       <div
         className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 `}
       >
-        <div className="flex flex-row items-center py-[1px] px-4 bg-discount-gradient rounded-[10px] mb-2 sm:mt-16">
+        <div className="flex flex-row items-center py-[1px] px-4 bg-discount-gradient rounded-[10px] mb-2 ">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
             <span className="text-white">Suplementi</span> za{" "}
@@ -20,11 +20,11 @@ const Hero = () => {
           </p>
         </div>
 
-        <div className="flex flex-row justify-between items-center w-full sm:mt-16 ">
+        <div className="flex flex-row justify-between items-center w-full  ">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
             <span className="text-gradient">Fertilsan M</span>{" "}
           </h1>
-          <div className="ss:flex hidden md:mr-6 mr-0 sm:mt-16">
+          <div className="ss:flex hidden md:mr-6 mr-0 ">
             <GetStarted />
           </div>
         </div>
