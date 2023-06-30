b43fe26d3edb2989c42ae390eb7a653ae278a090
iammirzet <82601696+iammirzet@users.noreply.github.com>
2023-06-30 08:16:10 +0200

    comm

--- src/components/HeroTwo.jsx
+++ src/components/HeroTwo.jsx
@@ -36,13 +36,13 @@ const HeroTwo = () => {
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
             <span className="text-white">Suplementi</span> za{" "}
-            <span className="text-white">Buduce</span> Oceve
+            <span className="text-white">Buduće</span> Očeve
           </p>
         </div>
 
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            <span className="text-gradient">Fertilsan M</span>{" "}
+            <span className="text-gradient">Fertilsan M (u prahu)</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-6 mr-0">
             <GetStarted />
