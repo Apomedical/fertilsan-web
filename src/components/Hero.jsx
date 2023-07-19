3d5a747272c2576a610118daa4fcdd9ca4a9a3a8
iammirzet <82601696+iammirzet@users.noreply.github.com>
2023-07-19 11:49:35 +0200

    r

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -40,7 +40,7 @@ const Hero = () => {
           astenozoospermija)
         </p>
         <br></br>
-        <Modal buttonText="Opširnije" imageSrc="{tabletePrah}" />
+        <Modal buttonText="Opširnije" imageSrc={tabletePrah} />
       </div>
 
       <div
